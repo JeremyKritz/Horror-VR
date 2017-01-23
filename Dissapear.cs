60a47eab6adc493eaad2b87c9b09a45db9288e1b
Jeremy Kritz <jaypanang@gmail.com>
2017-01-22 22:55:08 -0500

    First set of stuff

---
+++ Dissapear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissapear : MonoBehaviour {
+    private bool test = true;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Renderer r = col.gameObject.GetComponent<Renderer>();
+        if (r.enabled == true)
+        {
+
+            r.enabled = false;
+            
+        }
+        else {
+            r.enabled = true;
+         }
+    }
+	
+}
